75d05cd10ad8607745ccb289d78381978252d1d9
rocky <88kg@mail.ru>
2023-02-04 21:53:04 +0300

    add path vite

--- app/vite.config.js
+++ app/vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
     plugins: [
         vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/admin/admin.js',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
